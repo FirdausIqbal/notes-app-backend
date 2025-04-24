99bdddf440108be9483398d2c7f5c7850029455f
FirdausIqbal <firdausiqbal10@gmail.com>
2025-04-24 17:35:02 +0700

    Authorization implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,4 +1,3 @@
-
 class NotesHandler {
   constructor(service, validator) {
     this._service = service;
@@ -15,7 +14,14 @@ class NotesHandler {
     this._validator.validateNotePayload(request.payload);
     const { title = "untitled", body, tags } = request.payload;
 
-    const noteId = await this._service.addNote({ title, body, tags });
+    const { id: credentialId } = request.auth.credentials;
+
+    const noteId = await this._service.addNote({
+      title,
+      body,
+      tags,
+      owner: credentialId,
+    });
     const response = h.response({
       status: "success",
       message: "Catatan berhasil ditambahkan",
@@ -27,21 +33,31 @@ class NotesHandler {
     return response;
   }
 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes();
-    return {
-      status: "success",
-      data: notes,
-    };
+  async getNotesHandler(request, h) {
+    const { id: credentialId } = request.auth.credentials;
+    const notes = await this._service.getNotes(credentialId);
+    const response = h
+      .response({
+        status: "success",
+        data: {
+          notes
+        },
+      })
+      .code(200);
+    return response;
   }
 
   async getNoteByIdHandler(request, h) {
     const { id } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._service.verifyNoteOwner(id, credentialId);
+
     const note = await this._service.getNoteById(id);
     const response = h.response({
       status: "success",
       data: {
-        ...note,
+        note
       },
     });
     response.code(201);
@@ -52,6 +68,10 @@ class NotesHandler {
     this._validator.validateNotePayload(request.payload);
 
     const { id } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._service.verifyNoteOwner(id, credentialId);
+
     await this._service.editNoteById(id, request.payload);
     return {
       status: "success",
@@ -61,6 +81,10 @@ class NotesHandler {
 
   async deleteNoteByIdHandler(request, h) {
     const { id } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._service.verifyNoteOwner(id, credentialId);
+
     await this._service.deleteNoteById(id);
     return {
       status: "success",
--- src/services/postgres/NoteService.js
+++ src/services/postgres/NoteService.js
@@ -4,20 +4,21 @@ import pg from 'pg';
 import { InvariantError } from '../../exceptions/InvariantError.js';
 import { mapDBToModel } from '../../utils/index.js';
 import { NotFoundError } from '../../exceptions/NotFoundError.js';
+import { AuthorizationError } from '../../exceptions/AuthorizationError.js';
 
 export class NoteService {
     constructor() {
         this._pool = new pg.Pool()
     }
 
-    async addNote({title, body, tags}){
+    async addNote({title, body, tags, owner}){
         const id = nanoid(16);
         const createdAt = new Date().toISOString();
         const updateAt = createdAt;
 
         const query = {
-            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-            values: [id, title, body, tags, createdAt, updateAt]
+            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+            values: [id, title, body, tags, createdAt, updateAt, owner]
         }
 
         const result = await this._pool.query(query);
@@ -29,8 +30,12 @@ export class NoteService {
         return result.rows[0].id;
     }
 
-    async getNotes() {
-        const result = await this._pool.query('SELECT * FROM notes')
+    async getNotes(owner) {
+        const query = {
+            text: 'SELECT *FROM notes WHERE owner = $1',
+            values: [owner]
+        }
+        const result = await this._pool.query(query)
         return result.rows.map(mapDBToModel)
     }
 
@@ -73,6 +78,24 @@ export class NoteService {
 
         if(!result.rows.length) {
             throw new NotFoundError('Catatan tidak ditemukan')
+        }
+    }
+
+    async verifyNoteOwner(id, owner) {
+        const query = {
+            text: 'SELECT * FROM notes WHERE id = $1',
+            values: [id]
+        }
+
+        const result = await this._pool.query(query)
+
+        if(!result.rows.length){
+            throw new NotFoundError('Catatan tidak ditemukan')
+        }
+
+        const note = result.rows[0]
+        if(note.owner !== owner){
+            throw new AuthorizationError('Anda tidak berhak mengakses resource ini')
         }
     }
 }
